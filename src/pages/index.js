3eb65e2e32db48e35b7478e05e84d7d37da25a47
Edgar Dorausch <edgar@doraus.ch>
2020-06-09 17:17:40 +0200

    Added automatic page generation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,37 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function MyFiles({ data }) {
+  console.log(data)
+  return (<>
+      <div>My blog posts</div>
+      <ul>
+        {data.allMarkdownRemark.edges.map(foo => (<li>
+          <Link
+          to={foo.node.fields.slug}
+          >
+            {foo.node.frontmatter.title} - {foo.node.frontmatter.date}
+          </Link>
+          </li>))}
+      </ul>
+    </>
+  )
 }
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            date
+            title
+          }
+        }
+      }
+    }
+  }
+`
