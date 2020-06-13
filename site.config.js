98b79aec8325b85360a0923cd61c96bb349ce34c
Edgar Dorausch <edgar@doraus.ch>
2020-06-13 17:49:15 +0200

    Edited site configuration

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  title: `M@th B|og`,
+  title: `Edgar.Doraus.ch/Blog`,
   description: `The premier math blog starter for Gatsby.`,
-  author: `@kunalja`,
-  personalDescription: `I am a student at the University of Texas. I am an aspriring software engineer hoping to enter the space of distributed system design and systems programming.`,
+  author: `@EdgarDorausch`,
+  personalDescription: `I am a computer science student from Germany`,
 
   socialLinks: {
     facebook: 0,
     gatsby: 0,
-    github: `https://github.com/kunalJa/gatsby-starter-math-blog`,
+    github: `https://github.com/EdgarDorausch`,
     imdb: 0,
-    linkedin: `https://www.linkedin.com/in/kunalja`,
+    linkedin: `https://www.linkedin.com/in/EdgarDorausch/`,
     reddit: 0,
     skype: 0,
     slack: 0,
